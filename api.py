492c99b2e3d93806a69ad816b6b96b7de83338bb
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-05 21:02:14 +0700

    add docker compose to run airflow on docker

--- api.py
+++ api.py
@@ -4,12 +4,25 @@ from bs4 import BeautifulSoup
 
 
 url = "https://www.imdb.com/title/tt1517268/"
-header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
-res = requests.get(url, headers=header)
-# data = json.loads(res.content)
-data = res.content.decode("utf-8")
-# data_json = data.json()
-soup = BeautifulSoup(data, 'html.parser')
-title = soup.find_all("span")
-print(soup)
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+rows = soup.find_all("tr")
+fact_movie = []
+
+for row in rows[1:]:
+
+def extract_id(url: str):
+    res = requests.get(url)
+    id = find("...")
+    return id
+
+def crawl_box_office():
+    url = "https://www.boxofficemojo.com/date/1221"
+    href = "/release/..."
+    url_detail = url[:10] + href
+
+    id = extract_id(url_detail)
+
