a0c6a70aced55796a9e630b0f06ebe820fea705f
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-10-09 23:46:28 +0700

    read data from postgresql into spark dataframe

--- spark/ingestion.py
+++ spark/ingestion.py
@@ -3,28 +3,39 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .master('local')\
     .appName("Snowflake Data Lake Writer") \
-    .config("spark.jars", "./spark/utils/postgresql-42.6.0.jar")\
+    .config("spark.jars",
+             "./spark/lib/postgresql-42.6.0.jar, ./spark/lib/spark-snowflake_2.12-2.9.0-spark_3.1.jar")\
     .getOrCreate()
 
 
 jdbcDF = spark.read \
               .format("jdbc") \
-              .option("url", "jdbc:postgresql://localhost:5432/postgres") \
+              .option("url", "jdbc:postgresql://localhost:5434/postgres") \
               .option("dbtable", "movie_revenue") \
-              .option("user", "postgres") \
-              .option("password", "khang12102003") \
+              .option("user", "airflow") \
+              .option("password", "airflow") \
               .option("driver", "org.postgresql.Driver") \
               .load()
 
 jdbcDF.show()
 
 sfOptions = {
-    "sfURL": "https://app.snowflake.com/ytymwtq/bu51995/",
-    "sfAccount": "bu51995",
+    "sfURL": "ytymwtq-bu51995.snowflakecomputing.com",
+    "sfAccount": "BU51995",
     "sfUser": "khanghoang12",
     "sfPassword": "Khang12102003@",
-    "sfDatabase": "data_lake",
+    "sfAuthenticator": "oauth",
+    "sfDatabase": "DATA_LAKE",
     "sfSchema": "PUBLIC",
     "sfWarehouse": "COMPUTE_WH",
     "sfRole": "ACCOUNTADMIN"
 }
+
+SNOWFLAKE_SOURCE_NAME = "net.snowflake.spark.snowflake"
+
+jdbcDF.write\
+    .format(SNOWFLAKE_SOURCE_NAME)\
+    .options(**sfOptions)\
+    .option("dbtable", "TEST")\
+    .mode('append')\
+    .save()
