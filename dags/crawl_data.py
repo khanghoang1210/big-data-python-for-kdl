1d3ad69f8d5c36af9eb82314d7c82121bf5279d7
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-10-13 11:11:50 +0700

    refactor crawl imdb data code

--- dags/crawl_data.py
+++ dags/crawl_data.py
@@ -27,8 +27,10 @@ def boxOfficeMojo(date):
         final_data = {}
         # Find necessary data
         rank = row.find('td',{'class':'mojo-header-column'}).text
+        #nameMovie = row.find('td',{'class':'mojo-field-type-release'}).text
         revenue = row.find('td',{'class':'mojo-field-type-money'}).text
         gross_change_by_date = row.find('td',{'class':'mojo-field-type-percent_delta'}).text
+        #studio = row.find('td',{'class':'mojo-field-type-release_studios'}).text.replace('\n',"")
         url_detail = 'https://www.boxofficemojo.com/' + row.find('a',
                                          {'class':'a-link-normal'}).get('href')
         id_imdb = get_id_boxoffice(url_detail)
@@ -46,33 +48,50 @@ def crawl_imdb_data(**items):
     imdb_data = [] 
     context = items['ti'].xcom_pull(task_ids='crawl_fact_data')
     for item in context:
+        dim_items = {}
         id = item['id']
-        url = f"https://www.imdb.com/title/{id}/faq/"
+        url = f"https://www.imdb.com/title/{id}/"
 
         header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
         response = requests.get(url,headers=header)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        div = soup.find_all('div',{'class':'ipc-html-content-inner-div'})
-        data = list(div)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        dim_items["title"] = soup.find("span", {"class":"fDTGTb"}).text.replace("'","")
+        dim_items["movie_id"] = id
+        dim_items["url"] = url.replace("'","")
+        dim_items["director"] = soup.find("a", 
+                                          {"class": "ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"}).text.replace("'","")
+        #dim_items["crawled_date"] = date
+        imdb_data.append(dim_items)
+
+        #div = soup.find_all('div',{'class':'ipc-html-content-inner-div'})
         
+        #movie_duration = soup.find("li", {"id": "run-time"})#.find("div", {'class':"ipc-html-content-inner-div"})
+        #div_run_time = run_time.find("div", {'class':"ipc-html-content-inner-div"}).text
         # # Get neccessary data
-        movie_duration = data[0].text
-        rating = float(data[2].text.replace(" out of 10",""))
-        director = data[5].text
-        budget = data[12].text
-        worldwide = data[13].text
-        genre = (data[16].text).replace(" and","")
+        #title = soup.find('h2',{'data-testid': "subtitle"})
+        # movie_duration = div_run_time
+        # rating = div[2].text.split(" ")[0]
+        # director = div[5].text
+        # budget = div[12].text
+        # # worldwide = div[13].text
+        # # genre = (div[16].text).replace(" and","")
         
-        final_data = {}
-        final_data['id'] = id
-        final_data['movie_duration'] = movie_duration
-        final_data['rating'] = rating
-        final_data['director'] = director
-        final_data['budget'] = budget
-        final_data['worldwide'] = worldwide
-        final_data['genre'] = genre
-        imdb_data.append(final_data)
-   # print(imdb_data)
+
+
+        # final_data = {}
+        # final_data['title'] = title.text
+        # final_data['id'] = id
+        #final_data['movie_duration'] = soup
+        # final_data['rating'] = rating
+        # final_data['director'] = director
+        # final_data['budget'] = budget
+        # # final_data['worldwide'] = worldwide
+        # # final_data['genre'] = genre
+        # final_data['url'] = url
+
+        imdb_data.append(dim_items)
+        
     return imdb_data
 
 # if __name__ =='__main__':
