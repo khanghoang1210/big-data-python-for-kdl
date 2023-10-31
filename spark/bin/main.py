06131d79aa9494be059627c7a743c70ce4704769
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-10-31 17:55:28 +0700

    add function incremental loading data

--- spark/bin/main.py
+++ spark/bin/main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from dotenv import load_dotenv
 import os
-from ingestion import read_data_from_postgre, ingest_data
+from ingestion import read_data_from_postgre, ingest_data, create_snowflake_table
 from validations import df_count, df_print_schema ,df_top10_rec
 
 import logging
@@ -17,6 +17,17 @@ sfUser = os.getenv("sfUser")
 sfPassword = os.getenv("sfPassword")
 db_user = os.getenv("db_user")
 db_password = os.getenv("db_password")
+    # Define connection in Snowflake
+sfOptions = {
+"sfURL": sfURL,
+"sfAccount": sfAccount,
+"sfUser": sfUser,
+"sfPassword": sfPassword,
+"sfDatabase": "DATA_LAKE",
+"sfSchema": "PUBLIC",
+"sfWarehouse": "COMPUTE_WH",
+"sfRole": "ACCOUNTADMIN"
+}
 
 # create spark object
 try:
@@ -29,30 +40,22 @@ try:
                 "./spark/lib/postgresql-42.6.0.jar, ./spark/lib/spark-snowflake_2.12-2.12.0-spark_3.4.jar")\
         .getOrCreate()
     logging.info("Spark object is created.")
-    
+
+    create_snowflake_table(spark,sfOptions, "movie_revenue")
+    create_snowflake_table(spark, sfOptions, "movies_detail")
     # Reading data 
-    movie_revenue = read_data_from_postgre(spark, "movie_revenue", db_user, db_password)
+    movie_revenue = read_data_from_postgre(spark, "movie_revenue", db_user, db_password, sfOptions)
   
-    movies_detail = read_data_from_postgre(spark, "movies_detail", db_user, db_password)
+    movies_detail = read_data_from_postgre(spark, "movies_detail", db_user, db_password, sfOptions)
 
     # Validate data
     df_top10_rec(movie_revenue, "movie_revenue")
     df_count(movie_revenue, "movie_revenue")
+    
 
     df_top10_rec(movies_detail, "movies_detail")
     df_count(movies_detail, "movies_detail")
 
-    # Define connection in Snowflake
-    sfOptions = {
-    "sfURL": sfURL,
-    "sfAccount": sfAccount,
-    "sfUser": sfUser,
-    "sfPassword": sfPassword,
-    "sfDatabase": "DATA_LAKE",
-    "sfSchema": "PUBLIC",
-    "sfWarehouse": "COMPUTE_WH",
-    "sfRole": "ACCOUNTADMIN"
-    }
 
     # write movie_revenue data frame into data lake
     ingest_data(spark,sfOptions, movie_revenue, "movie_revenue")
