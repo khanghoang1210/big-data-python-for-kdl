03192fe96c866721ae765839fd319d955f5ad975
Jearann <jearann111003@gmail.com>
2023-12-16 10:35:33 +0700

    Edited Dashboard

--- dashboard/temp.py
+++ dashboard/temp.py
@@ -1,9 +1,21 @@
+import streamlit as st
+import pandas as pd
 
+data = {
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Edward'],
+    'height': [5.5, 6.1, 5.7, 6.0, 5.8],
+    'weight': [150, 170, 160, 180, 170]
+}
+df = pd.DataFrame(data)
 
+sort_column = st.selectbox('Select column to sort by:', df.columns[1:])
+df_sorted = df.sort_values(by=sort_column, ascending=False)
 
-with open('user_account.txt', 'r') as file:
-    account, user, password = file.readlines()
-    account = account.rstrip()
-    user = user.rstrip()
+st.bar_chart(df_sorted['height'])
 
-print(account, user ,password)
+st.dataframe(df_sorted)
+
+N = 20
+name_list = df_sorted['name'].head(N).tolist()
+
+st.write(name_list)
